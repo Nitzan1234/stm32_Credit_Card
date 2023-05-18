e96400fa1a58e9ee844bd72aaf90a2b6b5eb813e
Nitzan1234 <124502185+Nitzan1234@users.noreply.github.com>
2023-05-18 16:40:01 +0300

    Update tokenization.c

--- tokenization.c
+++ tokenization.c
@@ -2,6 +2,117 @@
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/sha.h>
+#include "stm32f4xx_hal.h"
+#include "st25r3911.h"
+#include <stdint.h>
+
+// Initialize the ST25R NFC module
+void ST25R3911_Init(void)
+{
+    // Initialize the SPI peripheral for communication with the ST25R3911
+    hspi.Instance = SPI1;
+    // Configure other SPI settings as needed
+    // ...
+    HAL_SPI_Init(&hspi);
+
+    // Configure the ST25R3911 module using the provided library
+    // ...
+}
+uint8_t NFC_SPI_ReceiveData(void)
+{
+    uint8_t receivedData = 0;
+
+    // Set the NSS (chip select) pin to low to select the NFC module
+    HAL_GPIO_WritePin(NSS_GPIO_Port, NSS_Pin, GPIO_PIN_RESET);
+
+    // Receive data from the NFC module
+    HAL_SPI_Receive(&hspi, &receivedData, 1, HAL_MAX_DELAY);
+
+    // Set the NSS (chip select) pin to high to deselect the NFC module
+    HAL_GPIO_WritePin(NSS_GPIO_Port, NSS_Pin, GPIO_PIN_SET);
+
+    return receivedData;
+}
+
+// Transmit data using the NDEF protocol
+void ST25R3911_TransmitNDEF(uint8_t* data, uint16_t length)
+{
+    // Check if the data length is within the ST25R3911's maximum buffer size
+    if (length > ST25R3911_MAX_BUFFER_SIZE)
+    {
+        // Data length exceeds the maximum buffer size
+        // Handle the error or return an appropriate error code
+        return;
+    }
+
+    // Create and initialize an NDEF message structure
+    NDEF_Message ndefMessage;
+    setupNDEFMessage(&ndefMessage, data, length);
+
+    // Convert the NDEF message structure into a byte array
+    uint8_t ndefBuffer[ST25R3911_MAX_BUFFER_SIZE];
+    uint16_t ndefBufferSize = 0;
+
+    // Add the message type to the NDEF buffer
+    ndefBuffer[ndefBufferSize++] = ndefMessage.messageType;
+
+    // Add the payload length to the NDEF buffer
+    ndefBuffer[ndefBufferSize++] = ndefMessage.payloadLength;
+
+    // Add the payload data to the NDEF buffer
+    for (uint16_t i = 0; i < ndefMessage.payloadLength; i++) {
+        ndefBuffer[ndefBufferSize++] = ndefMessage.payloadData[i];
+    }
+
+    // Configure the ST25R3911 module for transmission
+    // ...
+
+    // Set the transmission parameters, e.g., modulation, bitrate, etc.
+    // ...
+
+    // Send the NDEF message using the ST25R3911 module
+    ST25R3911_SendData(ndefBuffer, ndefBufferSize);
+}
+// Function to set the ST25R3911 NFC module to transparent mode
+void ST25R3911_SetTransparentMode(void)
+{
+    // Enable the SPI peripheral for communication with ST25R3911
+    __HAL_RCC_SPI1_CLK_ENABLE();
+    
+    // Configure the SPI interface
+    SPI_HandleTypeDef hspi;
+    hspi.Instance = SPI1;
+    hspi.Init.Mode = SPI_MODE_MASTER;
+    hspi.Init.Direction = SPI_DIRECTION_2LINES;
+    hspi.Init.DataSize = SPI_DATASIZE_8BIT;
+    hspi.Init.CLKPolarity = SPI_POLARITY_LOW;
+    hspi.Init.CLKPhase = SPI_PHASE_1EDGE;
+    hspi.Init.NSS = SPI_NSS_SOFT;
+    hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_64;
+    hspi.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    hspi.Init.TIMode = SPI_TIMODE_DISABLE;
+    hspi.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    hspi.Init.CRCPolynomial = 10;
+    if (HAL_SPI_Init(&hspi) != HAL_OK)
+    {
+        // Error occurred while initializing SPI
+        // Handle the error or return an appropriate error code
+        return;
+    }
+    
+    // Configure the GPIO pins for ST25R3911 control
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    
+    GPIO_InitTypeDef GPIO_InitStruct;
+    GPIO_InitStruct.Pin = NFC_TRANSPARENT_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(NFC_TRANSPARENT_GPIO_Port, &GPIO_InitStruct);
+    
+    // Set the ST25R3911 module to transparent mode
+    HAL_GPIO_WritePin(NFC_TRANSPARENT_GPIO_Port, NFC_TRANSPARENT_Pin, GPIO_PIN_SET);
+}
 
 // Function to compute the SHA-256 hash of the input data
 void computeHash(const unsigned char* input, size_t length, unsigned char* hashOutput)
@@ -13,7 +124,7 @@ void computeHash(const unsigned char* input, size_t length, unsigned char* hashO
 }
 
 // Function to generate a token from credit card details
-void tokenizeCreditCard(const char* creditCardNumber, const char* expiryDate, const char* cvv)
+long long unsigned int tokenizeCreditCard(const char* creditCardNumber, const char* expiryDate, const char* cvv)
 {
     // Perform tokenization logic here
     // This is a simplified example
@@ -24,10 +135,45 @@ void tokenizeCreditCard(const char* creditCardNumber, const char* expiryDate, co
 
     // Generate token
     unsigned char hash[SHA256_DIGEST_LENGTH];
-    computeHash((const unsigned char*) input, strlen(input), hash);
+    return computeHash((const unsigned char*) input, strlen(input), hash);
+}
+void NFC_Module_Init(void)
+{
+    // Initialize SPI interface for communication with the NFC module
+    hspi.Instance = SPI1; // Replace SPIx with the appropriate SPI instance (e.g., SPI1, SPI2, etc.)
+    hspi.Init.Mode = SPI_MODE_MASTER;
+    hspi.Init.Direction = SPI_DIRECTION_2LINES;
+    hspi.Init.DataSize = SPI_DATASIZE_8BIT;
+    hspi.Init.CLKPolarity = SPI_POLARITY_LOW;
+    hspi.Init.CLKPhase = SPI_PHASE_1EDGE;
+    hspi.Init.NSS = SPI_NSS_SOFT;
+    hspi.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
+    hspi.Init.FirstBit = SPI_FIRSTBIT_MSB;
+    hspi.Init.TIMode = SPI_TIMODE_DISABLE;
+    hspi.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+    hspi.Init.CRCPolynomial = 7;
+    HAL_SPI_Init(&hspi);
 
-    // Print the token
-    printf("Token: %u\n", token);
+    // Perform any additional initialization steps specific to the NFC module
+    // ...
+}
+
+void NFC_Module_Transmit(uint8_t command, uint8_t* data, uint8_t length)
+{
+    // Create transmit buffer
+    uint8_t txBuffer[8];
+    txBuffer[0] = command;
+    if (data != NULL)
+    {
+        // Copy data to transmit buffer
+        for (uint8_t i = 0; i < length; i++)
+        {
+            txBuffer[i + 1] = data[i];
+        }
+    }
+
+    // Send transmit buffer using SPI
+    HAL_SPI_Transmit(&hspi, txBuffer, length + 1, HAL_MAX_DELAY);
 }
 
 int main(void)
@@ -38,6 +184,9 @@ int main(void)
 
     // Generate token for credit card details
     tokenizeCreditCard(creditCardNumber, expiryDate, cvv);
-
+    // Example: Transmit data to the NFC module
+    NFC_Module_Transmit(NFC_CMD_WRITE, tokenizeCreditCard(creditCardNumber, expiryDate, cvv) , sizeof(tokenizeCreditCard(creditCardNumber, expiryDate, cvv)));
+    ST25R3911_SetTransparentMode();
+    ST25R3911_TransmitNDEF(NFC_SPI_ReceiveData(), sizeof(NFC_SPI_ReceiveData()));
     return 0;
-}
+    }
